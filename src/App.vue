4fc3556d3120ab7dac692569fa7b0603bd46de29
luisemm7 <7lchino7@gmail.com>
2020-06-05 10:50:08 -0500

    antes de agregar la appbar

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,100 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <v-app>
+    <v-app-bar
+      app
+      color="primary"
+      dark
+    >
+      <div class="d-flex align-center">
+        <v-img
+          alt="Vuetify Logo"
+          class="shrink mr-2"
+          contain
+          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
+          transition="scale-transition"
+          width="40"
+        />
+
+        <v-img
+          alt="Vuetify Name"
+          class="shrink mt-1 hidden-sm-and-down"
+          contain
+          min-width="100"
+          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
+          width="100"
+        />
+      </div>
+
+      <v-spacer></v-spacer>
+
+      <v-btn
+        href="https://github.com/vuetifyjs/vuetify/releases/latest"
+        target="_blank"
+        text
+      >
+        <span class="mr-2">Latest Release</span>
+        <v-icon>open_in_new</v-icon>
+      </v-btn>
+    </v-app-bar>
+
+    <v-content>
+      <HelloWorld/>
+    </v-content>
+
+    <!-- Footer ↓ -->
+
+      <v-footer
+      dark
+      padless
+      >
+        <v-card
+          class="flex"
+          flat
+          tile
+        >
+          <v-card-title class="amber lighten-2">
+            <strong class="subheading black--text">Visitanos en las siguientes plataformas!</strong>
+
+            <v-spacer></v-spacer>
+
+            <v-btn
+              v-for="icon in social"
+              :key="icon.icon"
+              class="mx-4 black--text"
+              icon
+              :href="icon.route" target="_blank"
+            >
+              <v-icon size="24px">{{ icon.icon }}</v-icon>
+            </v-btn>
+          </v-card-title>
+
+          <v-card-text class="py-2 gray--text text-center secondary">
+            {{ new Date().getFullYear() }} — <strong>Lencería Sunflower</strong>
+          </v-card-text>
+        </v-card>
+      </v-footer>
+
+    <!-- Footer ↑ -->
+
+  </v-app>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+import HelloWorld from './components/HelloWorld';
 
-#nav {
-  padding: 30px;
-}
+export default {
+  name: 'App',
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
+  components: {
+    HelloWorld,
+  },
 
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+  data: () => ({
+    social: [
+        {icon: 'mdi-facebook', route: 'https://www.facebook.com/pages/category/Clothing--Brand-/Lencer%C3%ADa-Sunflower-Underwear-519792908494757/'},
+        {icon: 'mdi-youtube', route: 'https://www.youtube.com/channel/UCWOZoA7FY1Ef_3FfaUBrreQ'},
+        {icon: 'mdi-instagram', route: 'https://instagram.com/lenceria_sunflowermx?igshid=1thhjx9kde20d'}
+      ],
+  }),
+};
+</script>
